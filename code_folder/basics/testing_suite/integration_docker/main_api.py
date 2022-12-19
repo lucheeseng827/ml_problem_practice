d9288be2d324e6df12d918a226f05c6208247b63
Nicholas Lu Chee Seng <lu.cheeseng827@gmail.com>
2022-12-19 11:58:52 +0800

    add integration test

---
+++ code_folder/basics/testing_suite/integration_docker/main_api.py
@@ -0,0 +1,15 @@
+import unittest
+import requests
+
+class TestAPI(unittest.TestCase):
+    def test_get(self):
+        r = requests.get("http://localhost:5000/users")
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.json(), {"users": ["John", "Jane"]})
+
+    def test_post(self):
+        r = requests.post("http://localhost:5000/users", json={"name": "Bob"})
+        self.assertEqual(r.status_code, 201)
+
+if __name__ == "__main__":
+    unittest.main()
