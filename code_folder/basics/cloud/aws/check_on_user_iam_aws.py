0881297dee449d0e054c346e8c67b553b47d09a7
Nicholas Lu Chee Seng <lu.cheeseng827@gmail.com>
2023-03-12 23:59:15 +0800

    add script to push golden image via tekton and ecs gcs

---
+++ code_folder/basics/cloud/aws/check_on_user_iam_aws.py
@@ -0,0 +1,19 @@
+import boto3
+
+iam = boto3.client('iam')
+
+# List of user names
+user_list = ['user1', 'user2', 'user3']
+
+# Iterate over each user in the list
+for user in user_list:
+    print(f'Roles for user {user}:')
+    response = iam.list_roles_for_instance_profile(
+        InstanceProfileName=user
+    )
+    roles = response['Roles']
+    if len(roles) == 0:
+        print('No roles found')
+    else:
+        for role in roles:
+            print(role['RoleName'])
