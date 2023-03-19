b18183de8761295de5608ea3ed5d53f6337ff031
Nicholas Lu Chee Seng <lu.cheeseng827@gmail.com>
2023-03-20 00:00:29 +0800

    add mlflow file

---
+++ 6_mlflow_versioning_intermediate_checkpointing.py
@@ -0,0 +1,66 @@
+import os
+import tempfile
+import mlflow
+import mlflow.tensorflow
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.callbacks import ModelCheckpoint
+from google.cloud import storage
+
+# Set up Google Cloud Storage
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "path/to/your/credentials.json"
+bucket_name = "your_bucket_name"
+storage_client = storage.Client()
+bucket = storage_client.get_bucket(bucket_name)
+
+# Load and preprocess the MNIST dataset
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+# Build the model
+model = Sequential([
+    Flatten(input_shape=(28, 28)),
+    Dense(128, activation='relu'),
+    Dropout(0.2),
+    Dense(10, activation='softmax')
+])
+
+# Compile the model
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Set up MLflow tracking and automatic logging
+mlflow.set_tracking_uri("http://localhost:5000")
+mlflow.set_experiment("mnist_experiment")
+mlflow.tensorflow.autolog()
+
+# Define a checkpoint callback
+checkpoint_dir = tempfile.mkdtemp()
+checkpoint_path = os.path.join(checkpoint_dir, "model_checkpoint_{epoch:02d}.hdf5")
+
+checkpoint_callback = ModelCheckpoint(
+    checkpoint_path,
+    save_freq='epoch',
+    verbose=1,
+    save_weights_only=True
+)
+
+# Train the model
+model.fit(
+    x_train, y_train,
+    epochs=5,
+    validation_data=(x_test, y_test),
+    callbacks=[checkpoint_callback]
+)
+
+# Upload model checkpoints to Google Cloud Storage
+for file in os.listdir(checkpoint_dir):
+    file_path = os.path.join(checkpoint_dir, file)
+    blob = bucket.blob(f"model_checkpoints/{file}")
+    blob.upload_from_filename(file_path)
+
+# Cleanup temporary checkpoint directory
+os.rmdir(checkpoint_dir)
---
+++ 6_mlflow_versioning_model.py
@@ -0,0 +1,26 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+# Load data and split into training and testing sets
+data = load_boston()
+X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.3, random_state=42)
+
+# Train the model
+model = RandomForestRegressor()
+model.fit(X_train, y_train)
+
+# Evaluate the model
+predictions = model.predict(X_test)
+mse = mean_squared_error(y_test, predictions)
+
+# Log the model with MLflow
+mlflow.set_tracking_uri("http://localhost:5000")
+mlflow.set_experiment("my_experiment")
+
+with mlflow.start_run():
+    mlflow.log_metric("mse", mse)
+    mlflow.sklearn.log_model(model, "model")
