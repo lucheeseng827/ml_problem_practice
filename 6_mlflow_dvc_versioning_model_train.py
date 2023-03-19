3bba849b88afda0796965e70577f17ea61a6656e
Nicholas Lu Chee Seng <lu.cheeseng827@gmail.com>
2023-03-20 00:01:28 +0800

    add mlflow file

---
+++ 6_mlflow_dvc_versioning_model_train.py
@@ -0,0 +1,50 @@
+"""
+These are in bash
+# For a local directory
+dvc remote add -d myremote /path/to/remote/directory
+
+# For Google Cloud Storage
+dvc remote add -d myremote gs://your-bucket-name/path
+
+# For Amazon S3
+dvc remote add -d myremote s3://your-bucket-name/path
+
+
+dvc add path/to/your/dataset
+dvc push
+
+
+
+git add .dvc/config path/to/your/dataset.dvc .gitignore
+git commit -m "Add dataset to DVC"
+git push
+
+"""
+
+
+
+import os
+import mlflow
+import mlflow.tensorflow
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+
+# Pull the dataset from DVC
+os.system("dvc pull path/to/your/dataset.dvc")
+
+# Load the dataset from the pulled DVC files
+(x_train, y_train), (x_test, y_test) = load_your_dataset_from_dvc_files()
+
+# Preprocess the dataset
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+# Build and compile the model
+# ...
+
+# Set up MLflow tracking and automatic logging
+# ...
+
+# Train the model
+# ...
