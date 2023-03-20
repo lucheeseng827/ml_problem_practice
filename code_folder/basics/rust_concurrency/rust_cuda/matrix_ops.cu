12e0b6366afc8888917dfb7484e8add33a37391a
Nicholas Lu Chee Seng <lu.cheeseng827@gmail.com>
2023-03-21 04:54:47 +0800

    cpp reformat

--- code_folder/basics/rust_concurrency/rust_cuda/matrix_ops.cu
+++ code_folder/basics/rust_concurrency/rust_cuda/matrix_ops.cu
@@ -1,22 +1,29 @@
-extern "C" {
+extern "C"
+{
 
-    __global__ void matrix_ops(const float* A, const float* B, float* dot, float* cross, int n) {
+    __global__ void matrix_ops(const float *A, const float *B, float *dot, float *cross, int n)
+    {
         int idx = blockIdx.x * blockDim.x + threadIdx.x;
 
-        if (idx < n * n) {
+        if (idx < n * n)
+        {
             int row = idx / n;
             int col = idx % n;
 
             dot[idx] = 0.0f;
 
-            for (int k = 0; k < n; k++) {
+            for (int k = 0; k < n; k++)
+            {
                 dot[idx] += A[row * n + k] * B[k * n + col];
             }
 
-            if (row < n && col < n && row != col) {
+            if (row < n && col < n && row != col)
+            {
                 cross[row * n + col] = A[row * n + (col + 1) % n] * B[(row + 1) % n * n + col] -
                                        A[row * n + col] * B[(row + 1) % n * n + (col + 1) % n];
-            } else {
+            }
+            else
+            {
                 cross[row * n + col] = 0;
             }
         }
