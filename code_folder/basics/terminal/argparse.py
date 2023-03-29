b7952d1d8ac36f27b66932df6e39ce0dcff50e00
Nicholas Lu Chee Seng <lu.cheeseng827@gmail.com>
2023-03-30 00:41:27 +0800

    update argparse 1,2

---
+++ code_folder/basics/terminal/argparse.py
@@ -0,0 +1,43 @@
+import argparse
+import subprocess
+import sys
+
+
+def list_instances(project, zone):
+    command = f"gcloud compute instances list --project {project} --zone {zone}"
+    result = subprocess.run(command, shell=True,
+                            text=True, stdout=subprocess.PIPE)
+    print(result.stdout)
+
+
+def create_instance(project, zone, instance_name):
+    command = f"gcloud compute instances create {instance_name} --project {project} --zone {zone}"
+    result = subprocess.run(command, shell=True,
+                            text=True, stdout=subprocess.PIPE)
+    print(result.stdout)
+
+
+parser = argparse.ArgumentParser(
+    description="A script to interact with Google Cloud SDK")
+
+parser.add_argument("-p", "--project",
+                    help="Google Cloud project ID", required=True)
+parser.add_argument("-z", "--zone", help="Google Cloud zone", required=True)
+
+subparsers = parser.add_subparsers(dest="command", required=True)
+
+list_parser = subparsers.add_parser(
+    "list", help="List Compute Engine instances")
+
+create_parser = subparsers.add_parser(
+    "create", help="Create a Compute Engine instance")
+create_parser.add_argument("instance_name", help="Name of the new instance")
+
+args = parser.parse_args()
+
+if args.command == "list":
+    list_instances(args.project, args.zone)
+elif args.command == "create":
+    create_instance(args.project, args.zone, args.instance_name)
+else:
+    sys.exit("Invalid command.")
---
+++ code_folder/basics/terminal/argparse2.py
@@ -0,0 +1,66 @@
+import argparse
+import subprocess
+import sys
+
+
+def list_ec2_instances(region):
+    command = f"aws ec2 describe-instances --region {region}"
+    result = subprocess.run(command, shell=True, text=True, stdout=subprocess.PIPE)
+    print(result.stdout)
+
+
+def create_ec2_instance(region, instance_type, ami_id):
+    command = f"aws ec2 run-instances --region {region} --instance-type {instance_type} --image-id {ami_id}"
+    result = subprocess.run(command, shell=True, text=True, stdout=subprocess.PIPE)
+    print(result.stdout)
+
+
+def list_virtual_machines(resource_group):
+    command = f"az vm list -g {resource_group} -o table"
+    result = subprocess.run(command, shell=True, text=True, stdout=subprocess.PIPE)
+    print(result.stdout)
+
+
+def create_virtual_machine(resource_group, vm_name, location):
+    command = f"az vm create -g {resource_group} -n {vm_name} --location {location} --image UbuntuLTS --generate-ssh-keys"
+    result = subprocess.run(command, shell=True, text=True, stdout=subprocess.PIPE)
+    print(result.stdout)
+
+
+parser = argparse.ArgumentParser(description="A script to interact with AWS CLI and Azure CLI")
+
+parser.add_argument("-p", "--provider", help="Cloud provider (aws or azure)", required=True)
+
+subparsers = parser.add_subparsers(dest="command", required=True)
+
+list_parser = subparsers.add_parser("list", help="List instances")
+list_parser.add_argument("-r", "--region", help="Region (for AWS) or resource group (for Azure)", required=True)
+
+create_parser = subparsers.add_parser("create", help="Create an instance")
+create_parser.add_argument("-r", "--region", help="Region (for AWS) or resource group (for Azure)", required=True)
+create_parser.add_argument("-n", "--name", help="Name of the new instance (for Azure only)")
+create_parser.add_argument("-i", "--image", help="Image ID (for AWS) or location (for Azure)")
+
+args = parser.parse_args()
+
+if args.provider == "aws":
+    if args.command == "list":
+        list_ec2_instances(args.region)
+    elif args.command == "create":
+        if not args.image:
+            sys.exit("Image ID is required for AWS.")
+        create_ec2_instance(args.region, args.image)
+    else:
+        sys.exit("Invalid command.")
+elif args.provider == "azure":
+    if args.command == "list":
+        list_virtual_machines(args.region)
+    elif args.command == "create":
+        if not args.name or not args.image:
+            sys.exit("Name and location are required for Azure.")
+        create_virtual_machine(args.region, args.name, args.image)
+    else:
+        sys.exit("Invalid command.")
+
+else:
+    sys.exit("Invalid provider.")
