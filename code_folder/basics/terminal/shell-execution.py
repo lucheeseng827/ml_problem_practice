ccccf1fb22d2caff90a7b2b16c55eeac97d0f146
Nicholas Lu Chee Seng <lu.cheeseng827@gmail.com>
2023-03-30 23:31:32 +0800

    add staleness check

---
+++ code_folder/basics/terminal/shell-execution.py
@@ -0,0 +1,18 @@
+# import subprocess
+
+# command = ['echo', 'Hello, World!']
+# result = subprocess.run(command, capture_output=True)
+# result = subprocess.run(command, capture_output=True, text=True)
+# # Access captured output
+# print(result.stdout)
+
+import subprocess
+import sys
+
+if sys.platform == 'win32':
+    command = ['cmd.exe', '/c', 'gcloud', 'projects', 'list', '--format', 'json']
+else:
+    command = ['echo', 'Hello, World!']
+
+result = subprocess.run(command, capture_output=True, text=True)
+print(result.stdout)
