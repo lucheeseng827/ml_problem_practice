35c654442aa7cb6cd374c2445610264805ff55ca
Nicholas Lu Chee Seng <lu.cheeseng827@gmail.com>
2023-03-30 23:44:04 +0800

    add test for staleness mechanism

---
+++ code_folder/basics/cloud/aws/staleness_mechanism_s3_object.py
@@ -0,0 +1,43 @@
+import boto3
+import datetime
+
+def lambda_handler(event, context):
+    s3 = boto3.client('s3')
+
+    # Get source bucket and key from the event
+    source_bucket = event['Records'][0]['s3']['bucket']['name']
+    source_key = event['Records'][0]['s3']['object']['key']
+
+    # Define the destination bucket
+    destination_bucket = 'your-destination-bucket'
+
+    # Get the MD5 hash of the source object
+    source_response = s3.head_object(Bucket=source_bucket, Key=source_key)
+    source_md5 = source_response['ETag'][1:-1]
+
+    try:
+        # Get the MD5 hash of the destination object
+        destination_response = s3.head_object(Bucket=destination_bucket, Key=source_key)
+        destination_md5 = destination_response['ETag'][1:-1]
+    except Exception as e:
+        print(f"Error getting destination object: {e}")
+        return
+
+    # If the MD5 hashes are the same, update the last_updated metadata
+    if source_md5 == destination_md5:
+        now = datetime.datetime.now().isoformat()
+        s3.copy_object(
+            CopySource={
+                'Bucket': destination_bucket,
+                'Key': source_key,
+            },
+            Bucket=destination_bucket,
+            Key=source_key,
+            MetadataDirective='REPLACE',
+            Metadata={
+                'last_updated': now,
+            },
+        )
+        print(f"Updated last_updated metadata of {source_key} to {now}")
+    else:
+        print(f"MD5 hashes are different, not updating metadata")
---
+++ code_folder/basics/cloud/aws/test_staleness_mechanism_s3_object.py
@@ -0,0 +1,46 @@
+import unittest
+import boto3
+from moto import mock_s3
+from staleness_lambda import lambda_handler
+
+
+class TestStalenessLambda(unittest.TestCase):
+
+    @mock_s3
+    def test_same_md5_hash_updates_metadata(self):
+        # Set up mock S3 buckets and objects
+        s3 = boto3.client('s3', region_name='us-east-1')
+        s3.create_bucket(Bucket='source-bucket')
+        s3.create_bucket(Bucket='destination-bucket')
+        s3.put_object(Bucket='source-bucket', Key='test.txt', Body='Hello, World!')
+        s3.put_object(Bucket='destination-bucket', Key='test.txt', Body='Hello, World!')
+
+        # Create a mock S3 event
+        event = {
+            'Records': [
+                {
+                    's3': {
+                        'bucket': {
+                            'name': 'source-bucket'
+                        },
+                        'object': {
+                            'key': 'test.txt'
+                        }
+                    }
+                }
+            ]
+        }
+
+        # Call the Lambda function
+        lambda_handler(event, None)
+
+        # Check if the last_updated metadata was updated
+        destination_response = s3.head_object(Bucket='destination-bucket', Key='test.txt')
+        self.assertIn('last_updated', destination_response['Metadata'])
+
+    # Add more test methods for different scenarios
+    # TODO: Add a test for when the MD5 hashes are different
+
+
+if __name__ == '__main__':
+    unittest.main()
