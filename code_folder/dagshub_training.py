9648699c4ed52a5268d811a35aa24bdf857c172f
Nicholas Lu Chee Seng <lu.cheeseng827@gmail.com>
2023-04-04 23:59:33 +0800

    add dag hub

---
+++ code_folder/dagshub_training.py
@@ -0,0 +1,29 @@
+import os
+import dvc.api
+import dagshub
+
+# Initialize DAGsHub experiment logger
+dagshub.init()
+
+# Define hyperparameters
+learning_rate = 0.1
+batch_size = 32
+num_epochs = 10
+
+# Load data using DVC
+with dvc.api.open('data/train.csv') as f:
+    train_data = f.read()
+
+with dvc.api.open('data/test.csv') as f:
+    test_data = f.read()
+
+# Preprocess data
+# ...
+
+# Define and train model
+# ...
+
+# Log metrics to DAGsHub
+with dagshub.dagshub_logger() as logger:
+    logger.log_hyperparams({'learning_rate': learning_rate, 'batch_size': batch_size, 'num_epochs': num_epochs})
+    logger.log_metrics({'accuracy': 0.95, 'loss': 0.05})
