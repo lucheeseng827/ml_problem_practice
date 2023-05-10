768acaf577f88fc980eaff992969387ee65f5c0c
Nicholas Lu Chee Seng <lu.cheeseng827@gmail.com>
2023-05-10 21:18:08 +0800

    update docker test and dockerfile

---
+++ code_folder/basics/docker/docker_test.py
@@ -0,0 +1,25 @@
+import docker
+
+def test_docker_image(image_name):
+    client = docker.from_env()
+
+    try:
+        container = client.containers.run(image_name, detach=True)
+        logs = container.logs(stream=True)
+
+        for log in logs:
+            print(log.decode().strip())
+
+        exit_code = container.wait()['StatusCode']
+        container.remove()
+
+        if exit_code == 0:
+            print("Image test passed.")
+        else:
+            print(f"Image test failed with exit code: {exit_code}")
+    except docker.errors.APIError as e:
+        print(f"Error while running the container: {e}")
+
+if __name__ == "__main__":
+    image_name = "your-docker-image:tag"
+    test_docker_image(image_name)
