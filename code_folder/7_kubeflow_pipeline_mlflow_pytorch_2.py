785a051207b8810799fa716ca6a241efa878bc48
Nicholas Lu Chee Seng <lu.cheeseng827@gmail.com>
2023-05-17 11:06:11 +0800

    update code for kubeflow

--- code_folder/7_kubeflow_pipeline_mlflow_pytorch_2.py
+++ code_folder/7_kubeflow_pipeline_mlflow_pytorch_2.py
@@ -5,7 +5,15 @@ from kfp import dsl
 from kubernetes.client import V1Toleration
 
 
-def train_with_pytorch(epochs: int, learning_rate: float, momentum: float, dropout: float, hidden_size: int, batch_size: int, mlflow_experiment_name: str):
+def train_with_pytorch(
+    epochs: int,
+    learning_rate: float,
+    momentum: float,
+    dropout: float,
+    hidden_size: int,
+    batch_size: int,
+    mlflow_experiment_name: str,
+):
     import torch
     import torch.nn as nn
     import torch.optim as optim
@@ -18,19 +26,23 @@ def train_with_pytorch(epochs: int, learning_rate: float, momentum: float, dropo
 
     # Define the transforms to apply to the data
     transform = transforms.Compose(
-        [transforms.ToTensor(),
-         transforms.Normalize((0.5,), (0.5,))])
+        [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
+    )
 
     # Load the training and test data
-    trainset = torchvision.datasets.MNIST(root='./data', train=True,
-                                          download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                              shuffle=True, num_workers=2)
+    trainset = torchvision.datasets.MNIST(
+        root="./data", train=True, download=True, transform=transform
+    )
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=batch_size, shuffle=True, num_workers=2
+    )
 
-    testset = torchvision.datasets.MNIST(root='./data', train=False,
-                                         download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                             shuffle=False, num_workers=2)
+    testset = torchvision.datasets.MNIST(
+        root="./data", train=False, download=True, transform=transform
+    )
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=batch_size, shuffle=False, num_workers=2
+    )
 
     # Define the neural network
     net = Net(hidden_size, dropout)
@@ -41,7 +53,9 @@ def train_with_pytorch(epochs: int, learning_rate: float, momentum: float, dropo
     optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=momentum)
 
     # Start the MLflow run
-    with mlflow.start_run(experiment_id=mlflow_experiment_name, run_name="pytorch-training") as run:
+    with mlflow.start_run(
+        experiment_id=mlflow_experiment_name, run_name="pytorch-training"
+    ) as run:
         # Log the parameters
         mlflow.log_param("epochs", epochs)
         mlflow.log_param("learning_rate", learning_rate)
@@ -55,7 +69,6 @@ def train_with_pytorch(epochs: int, learning_rate: float, momentum: float, dropo
 
         # Train the network
         for epoch in range(epochs):  # loop over the dataset multiple times
-
             running_loss = 0.0
             for i, data in enumerate(trainloader, 0):
                 # get the inputs; data is a list of [inputs, labels]
@@ -76,16 +89,96 @@ def train_with_pytorch(epochs: int, learning_rate: float, momentum: float, dropo
                 # print statistics
                 running_loss += loss.item()
                 if i % 2000 == 1999:  # print every 2000 mini-batches
-                    print('[%d, %5d] loss: %.3f' %
-                          (epoch + 1, i + 1, running_loss / 2000))
+                    print(
+                        "[%d, %5d] loss: %.3f" % (epoch + 1, i + 1, running_loss / 2000)
+                    )
                     mlflow.log_metric("training_loss", running_loss / 2000)
                     running_loss = 0.0
 
-        print('Finished Training')
+        print("Finished Training")
 
         # Test the network
         correct = 0
         total = 0
         with torch.no_grad():
             for data in testloader:
-                images, labels =
+                images, labels = data
+
+                images, labels = images.to(device), labels.to(device)
+                outputs = net(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+        accuracy = 100 * correct / total
+        print("Accuracy on test images: %d %%" % accuracy)
+        mlflow.log_metric("test_accuracy", accuracy)
+
+
+@dsl.pipeline(
+    name="PyTorch Training Pipeline",
+    description="A pipeline that trains a PyTorch neural network on MNIST dataset",
+)
+def pytorch_training_pipeline(
+    epochs: int = 10,
+    learning_rate: float = 0.001,
+    momentum: float = 0.9,
+    dropout: float = 0.5,
+    hidden_size: int = 128,
+    batch_size: int = 64,
+    mlflow_experiment_name: str = "pytorch-training-experiment",
+):
+    train = train_with_pytorch(
+        epochs,
+        learning_rate,
+        momentum,
+        dropout,
+        hidden_size,
+        batch_size,
+        mlflow_experiment_name,
+    )
+
+    pod_envs = [
+        {
+            "name": "MLFLOW_TRACKING_URI",
+            "value": "http://mlflow-tracking-server:5000",
+        },
+        {
+            "name": "MLFLOW_S3_ENDPOINT_URL",
+            "value": "http://minio:9000",
+        },
+        {
+            "name": "AWS_ACCESS_KEY_ID",
+            "value": "minio",
+        },
+        {
+            "name": "AWS_SECRET_ACCESS_KEY",
+            "value": "minio123",
+        },
+        {
+            "name": "AWS_REGION",
+            "value": "us-east-1",
+        },
+    ]
+
+    tolerations = [
+        V1Toleration(key="key", operator="Equal", value="value", effect="NoSchedule")
+    ]
+
+    train.apply(
+        mlflow.pytorch(
+            use_conda=True,
+            conda_channels=["conda-forge"],
+            conda_dependencies=["pytorch", "torchvision"],
+        )
+    )
+
+    train.set_node_selector_expression("kubernetes.io/os", "linux")
+    train.set_gpu_limit(1)
+    train.set_pod_envs(pod_envs)
+    train.set_pod_tolerations(tolerations)
+
+
+if __name__ == "main":
+    kfp.compiler.Compiler().compile(
+        pytorch_training_pipeline, "pytorch_training_pipeline.yaml"
+    )
