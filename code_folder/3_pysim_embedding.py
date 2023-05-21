400c28de8b5dfb9e43dbed1713251f2e09b48989
Nicholas Lu Chee Seng <lu.cheeseng827@gmail.com>
2023-05-22 04:21:32 +0800

    update gensim file

--- code_folder/3_pysim_embedding.py
+++ code_folder/3_pysim_embedding.py
@@ -1,10 +1,38 @@
 from gensim.models import Word2Vec
+import pandas as pd
 
+# Assuming you have a list of documents obtained from Elasticsearch results
 documents = [result["_source"]["field_name"] for result in results["hits"]["hits"]]
 
+# Train the Word2Vec model
 model = Word2Vec(documents, size=100, window=5, min_count=1)
 
+# Get the word embeddings
+embeddings = model.wv
 
-# after embedding, you can store into data store  like mysql or panda
+# Store the embeddings in a secondary data store, such as a database or file system
+# Option 1: Storing embeddings in a pandas DataFrame and saving to a CSV file
+embedding_df = pd.DataFrame(embeddings.vectors, index=embeddings.index2word)
+embedding_df.to_csv('word_embeddings.csv')
 
-"""Store the embeddings in a secondary data store, such as a database or file system. You can use libraries like pandas or PyMySQL to write the embeddings to a database, or pickle to save them to a file."""
+# Option 2: Storing embeddings in a MySQL database using PyMySQL
+import pymysql
+
+# Connect to the MySQL database
+connection = pymysql.connect(host='localhost', user='your_username', password='your_password', db='your_database')
+
+# Create a table to store the embeddings
+table_name = 'word_embeddings'
+with connection.cursor() as cursor:
+    create_table_query = f"CREATE TABLE IF NOT EXISTS {table_name} (word VARCHAR(255) PRIMARY KEY, embedding BLOB)"
+    cursor.execute(create_table_query)
+
+# Insert the embeddings into the database
+with connection.cursor() as cursor:
+    for word, embedding in zip(embeddings.index2word, embeddings.vectors):
+        insert_query = f"INSERT INTO {table_name} (word, embedding) VALUES (%s, %s)"
+        cursor.execute(insert_query, (word, embedding.tobytes()))
+
+# Commit the changes and close the connection
+connection.commit()
+connection.close()
