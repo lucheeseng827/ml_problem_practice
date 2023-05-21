2ec2ade57248ae7335200c534122b3c7fc154fc8
nick lu <lu.cheeseng827@gmail.com>
2023-05-21 17:04:48 +0800

    add ray train code from ray

--- code_folder/1_ray_training_dataset.py
+++ code_folder/1_ray_training_dataset.py
@@ -2,8 +2,6 @@ import torch
 import torch.nn as nn
 
 import ray
-from ray import train
-from ray.air import session, Checkpoint
 from ray.train.torch import TorchTrainer
 from ray.air.config import ScalingConfig
 
@@ -12,26 +10,53 @@ from ray.air.config import ScalingConfig
 use_gpu = False
 
 
-input_size = 1
-layer_size = 15
-output_size = 1
-num_epochs = 3
-
-
 class NeuralNetwork(nn.Module):
-    def __init__(self):
+    """
+    Neural network model for regression.
+
+    Args:
+        input_size (int): Size of the input features.
+        layer_size (int): Size of the hidden layer.
+        output_size (int): Size of the output.
+    """
+
+    def __init__(self, input_size, layer_size, output_size):
         super(NeuralNetwork, self).__init__()
         self.layer1 = nn.Linear(input_size, layer_size)
         self.relu = nn.ReLU()
         self.layer2 = nn.Linear(layer_size, output_size)
 
     def forward(self, input):
+        """
+        Forward pass of the neural network.
+
+        Args:
+            input (torch.Tensor): Input tensor.
+
+        Returns:
+            torch.Tensor: Output tensor.
+        """
         return self.layer2(self.relu(self.layer1(input)))
 
 
 def train_loop_per_worker():
+    """
+    Training loop for each worker.
+
+    This function is executed by each worker during training.
+
+    It performs the forward and backward passes, updates the model parameters,
+    and reports the training progress.
+
+    Raises:
+        ValueError: If the dataset shard is not found.
+    """
+
     dataset_shard = session.get_dataset_shard("train")
-    model = NeuralNetwork()
+    if dataset_shard is None:
+        raise ValueError("Dataset shard 'train' not found.")
+
+    model = NeuralNetwork(input_size=1, layer_size=15, output_size=1)
     loss_fn = nn.MSELoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
 
