c2a0fe7079a162472f326b77983cf673257e2893
Nicholas Lu Chee Seng <lu.cheeseng827@gmail.com>
2023-05-22 04:13:10 +0800

    update pytorch training code and xgb feature engineering

---
+++ code_folder/4_torch_data_train_model.py
@@ -0,0 +1,93 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+# Define the feature engineering steps
+def feature_engineering(data):
+    # Perform any necessary preprocessing steps here
+    scaler = StandardScaler()
+    scaled_data = scaler.fit_transform(data)
+    return scaled_data
+
+# Define the neural network model
+class Model(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(Model, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        return out
+
+# Load the tabular data
+data = load_tabular_data()  # Replace with your own data loading code
+
+# Perform feature engineering
+processed_data = feature_engineering(data)
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(
+    processed_data[:, :-1],  # Features
+    processed_data[:, -1],   # Target
+    test_size=0.2,
+    random_state=42
+)
+
+# Convert data to PyTorch tensors
+X_train = torch.tensor(X_train, dtype=torch.float32)
+X_test = torch.tensor(X_test, dtype=torch.float32)
+y_train = torch.tensor(y_train, dtype=torch.float32)
+y_test = torch.tensor(y_test, dtype=torch.float32)
+
+# Create DataLoader for training and testing sets
+train_dataset = TensorDataset(X_train, y_train)
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+test_dataset = TensorDataset(X_test, y_test)
+test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+
+# Define the hyperparameters
+input_size = X_train.shape[1]
+hidden_size = 64
+output_size = 1
+learning_rate = 0.001
+num_epochs = 10
+
+# Instantiate the model
+model = Model(input_size, hidden_size, output_size)
+
+# Define the loss function and optimizer
+criterion = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+# Training loop
+for epoch in range(num_epochs):
+    for inputs, labels in train_loader:
+        optimizer.zero_grad()
+
+        # Forward pass
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+
+        # Backward pass and optimization
+        loss.backward()
+        optimizer.step()
+
+    # Print the loss after each epoch
+    print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item()}")
+
+# Evaluation
+with torch.no_grad():
+    model.eval()
+    test_loss = 0
+    for inputs, labels in test_loader:
+        outputs = model(inputs)
+        test_loss += criterion(outputs, labels).item()
+
+    print(f"Test Loss: {test_loss/len(test_loader)}")
