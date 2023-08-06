adb1c2b5258af49a2baa388e5b79476a5fb3a8c4
Nicholas Lu Chee Seng <lu.cheeseng827@gmail.com>
2023-08-07 02:36:35 +0800

    Update check_labels_k8s_object.py

--- code_folder/basics/kubernetes/check_labels_k8s_object.py
+++ code_folder/basics/kubernetes/check_labels_k8s_object.py
@@ -3,6 +3,8 @@ from kubernetes import client, config
 
 
 def list_pod_labels(namespace):
+    file_name = "pod_labels_{}.yaml".format(namespace)
+
     config.load_kube_config()
 
     api = client.CoreV1Api()
@@ -10,9 +12,9 @@ def list_pod_labels(namespace):
     pod_list = api.list_namespaced_pod(namespace)
     labels = {pod.metadata.name: pod.metadata.labels for pod in pod_list.items}
 
-    with open("pod_labels.yaml", "w") as outfile:
+    with open(file_name, "w") as outfile:
         yaml.dump(labels, outfile, default_flow_style=False)
 
 
 if __name__ == "__main__":
-    list_pod_labels("default")
+    list_pod_labels("argo")
