57f6656cf8915dba2881bc60d2cbd4aeaa068b38
nick lu <lu.cheeseng827@gmail.com>
2023-08-12 15:06:26 +0800

    adding sample untested jax code

--- code_folder/1_sklearn_bayes_insurance_method.py
+++ code_folder/1_sklearn_bayes_insurance_method.py
@@ -1,26 +1,28 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import LabelEncoder
 
 # Load the dataset
-df = pd.read_csv('insurance.csv')
+df = pd.read_csv("insurance.csv")
 
-# Let's say we have a binary target variable 'claim' 
+# Let's say we have a binary target variable 'claim'
 # which indicates whether a claim was filed (1) or not (0)
-y = df['claim']
+y = df["claim"]
 
 # The remaining columns are our features
-X = df.drop('claim', axis=1)
+X = df.drop("claim", axis=1)
 
 # Convert categorical variables into numeric variables
 label_encoder = LabelEncoder()
-for col in X.select_dtypes(include=['object']).columns:
+for col in X.select_dtypes(include=["object"]).columns:
     X[col] = label_encoder.fit_transform(X[col])
 
 # Split the dataset into a training set and a test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
 # Create a Gaussian Naive Bayes object
 gnb = GaussianNB()
@@ -32,6 +34,6 @@ gnb.fit(X_train, y_train)
 y_pred = gnb.predict(X_test)
 
 # Print the accuracy score and the confusion matrix
-print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
-print('Confusion Matrix:')
+print("Accuracy: %.2f" % accuracy_score(y_test, y_pred))
+print("Confusion Matrix:")
 print(confusion_matrix(y_test, y_pred))
